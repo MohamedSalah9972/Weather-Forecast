1af075a63a1fed587c2347cb20f5fc144a12310f
MohamedSalah9972 <mohamed.salah9972@gmail.com>
2021-04-26 04:08:51 +0200

    edit root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get 'weather', to: 'weather#index'
+  root "weather#index"
   resources :weather
-  get 'search', to: 'weather#search'
-
+  get 'search', action: :search, controller: 'weather'
 end
